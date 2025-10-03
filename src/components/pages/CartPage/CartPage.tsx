2c8ddcfa2e8cbfaf8dec46f1e948089943e6242e
annadarko <annannatolievna1303@gmail.com>
2025-10-03 10:51:18 +0300

    Добавила hook для Title

--- src/components/pages/CartPage/CartPage.tsx
+++ src/components/pages/CartPage/CartPage.tsx
@@ -1,7 +1,9 @@
 import { Cart } from "components/cart"
+import { useTitle } from "hooks/useTitle"
 
 
 export const CartPage = () => {
+    useTitle ("My cart | Goods4you")
     return (
         <Cart />
     )
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,9 +3,11 @@ import { NavBar } from "components/UI/NavBar";
 import { Catalog } from "components/catalog";
 import { FAQ } from 'components/Faq/';
 import { Footer } from 'components/UI/footer';
+import { useTitle } from 'hooks/useTitle';
 
 
 export const Home = () => {
+    useTitle ("Catalog | Goods4you")
     return (
         <>
             <NavBar />
